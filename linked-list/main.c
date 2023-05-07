f1db48998b7f1d0cc7214f899abe7edb177827ab
Anthony Erbe <anthony.erbe@ecopoint-inc.com>
2023-05-07 11:24:38 -0500

    updates

--- linked-list/main.c
+++ linked-list/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+    int value;
+    struct node_t *next;
+} node_t;
+
+void printNodes(node_t *head) {
+    node_t *tmp = head;
+    while(tmp != NULL) {
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+node_t *createNode(int value) {
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+}
+
+void insertNodeAtHead(node_t **head, node_t *n) {
+    n->next = *head;
+    *head = n;
+}
 
 int main(void) {
-    printf("hello world\n");
+    node_t *head = NULL;
+
+    insertNodeAtHead(&head, createNode(10));
+    insertNodeAtHead(&head, createNode(20));
+    insertNodeAtHead(&head, createNode(30));
+
+    printNodes(head);
+
     return 0;
 }
